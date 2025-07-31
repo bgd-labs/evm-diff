b19edbd656a0559686be90833a783f12b0f6c871
Harsh Pandey <harshsatishpandey@gmail.com>
2025-07-31 20:22:26 +0400

    chore: remove bad rpc

--- script/index.ts
+++ script/index.ts
@@ -137,6 +137,8 @@ function selectRpcUrls(rpcUrls: string[]): string[] {
 		'cloudflare', // Requested resource not found.
 		'api.avax.network', // Unrecognized token '<', 429's easily.
 		'rpc.linea.build', // All error messages are identical so can't distinguish opcode support.
+		'https://rpc.ankr.com/',
+		'https://api.securerpc.com/v1', // internal server error
 	];
 	const filteredRpcUrls = rpcUrls.filter(
 		(url) => !domainsToSkip.some((domain) => url.includes(domain)),
